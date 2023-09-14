70cbec0517a93ada96ea21f44db7013827749ffd
Irfan <irfaanacar@gmail.com>
2023-09-14 17:27:25 +0300

    CSharp  text document applications
    This time, I coded how to load data into a text document via a form, how to load data into the form, and how to create a text document on the desktop. By The Way, sorry for my English.

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace MetinBelgesiUygulamalari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.Font = new System.Drawing.Font("Oswald", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(140, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BELGE ADI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.Font = new System.Drawing.Font("Oswald", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(125, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BELGE YOLU:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightGray;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(269, 65);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 39);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightGray;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(269, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(264, 39);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.LightGray;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(269, 296);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(244, 39);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "Seçilen metin belgesi ismi burada gözükür.";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Khaki;
+            this.button1.Font = new System.Drawing.Font("Oswald", 9.8F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(539, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "YOL SEÇ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
+            this.button2.Font = new System.Drawing.Font("Oswald", 11.8F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(328, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 62);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "KAYDET";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.button3.Font = new System.Drawing.Font("Oswald", 8.8F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(57, 555);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 62);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "LISTBOX\'A AKTAR";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkOrange;
+            this.button4.Font = new System.Drawing.Font("Oswald", 8.8F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(328, 555);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 62);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "RICH TEXTBOX\'A AKTAR";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightGreen;
+            this.button5.Font = new System.Drawing.Font("Oswald", 8.8F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(589, 555);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 62);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "VERİLERİ KAYDET";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Silver;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 341);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(244, 208);
+            this.listBox1.TabIndex = 11;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(269, 341);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(244, 208);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.Plum;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.richTextBox2.Location = new System.Drawing.Point(529, 341);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(244, 208);
+            this.richTextBox2.TabIndex = 13;
+            this.richTextBox2.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(785, 629);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetinBelgesiUygulamalari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        StreamWriter sw;
+        string belgeadi, belgeyolu;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            belgeadi = textBox1.Text; //isimlendireceğim belgenin ismidir.
+            sw = File.CreateText(belgeyolu+"\\"+belgeadi+".txt"); //bu satırda ilgili konuma belge kaydetme işlemini gerçekleştirir.       
+            sw.Close();
+            MessageBox.Show("Belgeniz oluşturuldu.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)//burası buton3 e tıklandığında açılan kısımdır ve eğer bir belge seçilirse;
+            {
+                StreamReader oku = new StreamReader(openFileDialog1.FileName); //dosyayı okusun.
+                string satir = oku.ReadLine();//okuduğu verileri satir adlı değişkene aktarsın.
+                while(satir!=null)//ve okuduğu verileri null değerine ulaşana kadar okumaya devam etsin.
+                {
+                    listBox1.Items.Add(satir);//veriler listbox'a ekleniyor ancak hafızada.
+                    satir = oku.ReadLine();//ve hafızayı okuma kısmı.
+
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ac = new OpenFileDialog();
+            ac.Filter = "Metin Dosyası(*.txt) | *.txt";
+            ac.Multiselect = false;
+            if (ac.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox3.Text = ac.SafeFileName;
+                try
+                {
+                    StreamReader oku = new StreamReader(ac.FileName);
+                    richTextBox1.Text= oku.ReadToEnd();
+                    oku.Close();
+                    MessageBox.Show("Veriler başarıyla eklendi.");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Bu dosyayı okurken bir hata oluştu.");
+                  
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           textBox3.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.Title = "Kayıt Yerini Seçin"; //Klasör seçmek için açılan görünümün başlığını belirler.
+                saveFileDialog1.Filter = "Metin Dosyası(*.txt) | *.txt"; //Yalnızca metin belgesi şeklinde kaydeder.
+                saveFileDialog1.FilterIndex = 2; //yukarıdaki kod satırındaki ikinci indisi seçer.(FilterIndisler 1. indisten başlar). 2.indisi seçtiği için dosya uzantısı [.txt] olur.
+                saveFileDialog1.InitialDirectory = "C:\\"; //İlk açıldığında C klasöründen başlatmasını istedik.
+                saveFileDialog1.ShowDialog(); //Ve diyalog açıldı.
+                StreamWriter kaydet= new StreamWriter(saveFileDialog1.FileName); // save edilecek dosyayı kaydet değişkenine aktardık.
+                kaydet.WriteLine(richTextBox2.Text); // richtextbox2'den gelen değerler kaydet adlı değişkene gider.
+                kaydet.Close();
+                MessageBox.Show("Kaynak Metin Belgesine Yazdırıldı.");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bilinmeyen bir hatadan ötürü işlem gerçekleşmedi.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                belgeyolu = folderBrowserDialog1.SelectedPath.ToString();//Seçtiğimiz yolu belgeyolu adlı değişkene aktardık.
+                textBox2.Text=belgeyolu.ToString(); //textbox2'ye belgeyolunu yazdırdık.
+            }
+        }
+    }
+}
